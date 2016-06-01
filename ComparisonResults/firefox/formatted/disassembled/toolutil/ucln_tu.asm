c8093337ec0602297ac61321ddf891518352a9bd
SaketaChalamchala <saketa.chalamchala@gmail.com>
2016-06-01 13:36:05 -0400

    Add files via upload

---
+++ ComparisonResults/firefox/formatted/disassembled/toolutil/ucln_tu.asm
@@ -0,0 +1,25 @@
+endp
+?uprv_dummyFunction_TU@@YAHXZ proc near
+push ['ebp']
+mov ['ebp,', 'esp']
+sub ['esp,', '0C0h']
+push ['ebx']
+push ['esi']
+push ['edi']
+lea ['edi,', '[ebp+var_C0]']
+mov ['ecx,', '30h']
+mov ['eax,', '0CCCCCCCCh']
+rep ['stosd']
+xor ['eax,', 'eax']
+pop ['edi']
+pop ['esi']
+pop ['ebx']
+mov ['esp,', 'ebp']
+pop ['ebp']
+retn
+align ['4']
+assume ['cs:_rtc$TMZ']
+assume ['cs:_rtc$IMZ']
+extrn ['__RTC_Shutdown:near']
+extrn ['__RTC_InitBase:near']
+end
