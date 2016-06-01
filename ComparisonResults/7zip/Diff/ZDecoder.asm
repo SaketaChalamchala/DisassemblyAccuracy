016d1c4b608bea9168baf3d7c91094c64744f93b
SaketaChalamchala <saketa.chalamchala@gmail.com>
2016-06-01 11:56:09 -0400

    Add files via upload

---
+++ ComparisonResults/7zip/Diff/ZDecoder.asm
@@ -0,0 +1,13 @@
+_==@8
+original code :
+push|mov|mov|push|mov|push|call|cmp|call|pop|ret|
+disassembled code :
+
+end _==@8
+?Code@CDecoder@NZ@NCompress@@UAGJPAUISequentialInStream@@PAUISequentialOutStream@@PB_K2PAUICompressProgressInfo@@@Z
+original code :
+push|mov|push|push|mov|push|push|sub|push|push|push|lea|mov|mov|rep|mov|xor|push|lea|mov|mov|mov|mov|push|mov|push|mov|push|mov|push|mov|push|mov|call|mov|mov|jmp|mov|mov|mov|mov|mov|ret|mov|ret|mov|mov|mov|mov|mov|ret|mov|ret|mov|mov|mov|ret|mov|ret|mov|jmp|mov|jmp|jmp|mov|jmp|jmp|mov|push|mov|push|lea|call|pop|pop|mov|mov|pop|pop|pop|pop|add|cmp|call|mov|pop|ret|
+disassembled code :
+push|mov|push|push|mov|push|push|sub|push|push|push|lea|mov|mov|rep|mov|xor|push|lea|mov|mov|mov|mov|push|mov|push|mov|push|mov|push|mov|push|mov|call|mov|mov|jmp|
+
+end ?Code@CDecoder@NZ@NCompress@@UAGJPAUISequentialInStream@@PAUISequentialOutStream@@PB_K2PAUICompressProgressInfo@@@Z
