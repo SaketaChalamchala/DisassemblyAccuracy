b8b14ddda8a93e3d427f31e167e3cb0732919df4
SaketaChalamchala <saketa.chalamchala@gmail.com>
2016-06-01 13:25:29 -0400

    Add files via upload

---
+++ ComparisonResults/firefox/Diff/common/uchar.asm
@@ -0,0 +1,6 @@
+_u_isWhitespace_56
+original code :
+push|mov|sub|push|push|push|mov|mov|rep|cmp|jae|mov|sar|mov|movzx|mov|and|mov|jmp|cmp|ja|xor|cmp|setg|sub|and|mov|sar|add|mov|movzx|mov|and|mov|jmp|cmp|mov|add|mov|jmp|mov|cmp|jl|mov|mov|jmp|mov|sar|mov|movzx|mov|sar|and|add|mov|movzx|mov|and|mov|mov|mov|mov|mov|mov|mov|mov|mov|movzx|mov|mov|and|mov|shl|and|je|cmp|je|cmp|je|cmp|jne|cmp|jg|cmp|jl|cmp|jle|cmp|jge|mov|jmp|mov|mov|pop|pop|pop|mov|pop|ret|
+disassembled code :
+
+end _u_isWhitespace_56
