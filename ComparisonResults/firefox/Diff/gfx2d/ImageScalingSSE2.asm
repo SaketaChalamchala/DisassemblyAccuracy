51a49c0949c3392fadce693d940769f8aaaf32cb
SaketaChalamchala <saketa.chalamchala@gmail.com>
2016-06-01 13:23:29 -0400

    Add files via upload

---
+++ ComparisonResults/firefox/Diff/gfx2d/ImageScalingSSE2.asm
@@ -0,0 +1,7 @@
+?_Copy@?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAEXII@Z
+original code :
+push|mov|push|push|mov|push|push|sub|push|push|push|push|mov|mov|rep|pop|mov|xor|push|mov|mov|mov|mov|or|mov|mov|call|cmp|jae|mov|mov|jmp|mov|xor|mov|div|mov|mov|shr|cmp|ja|jmp|mov|mov|shr|mov|call|sub|mov|cmp|ja|mov|mov|shr|mov|add|mov|jmp|mov|call|mov|mov|push|mov|call|mov|mov|add|push|mov|call|mov|mov|mov|jmp|mov|mov|mov|mov|push|mov|call|mov|mov|add|push|mov|call|mov|mov|mov|jmp|push|push|mov|call|push|push|call|mov|ret|mov|jmp|mov|mov|ret|mov|jmp|mov|cmp|mov|push|mov|call|push|mov|push|call|add|push|push|mov|call|push|mov|add|push|push|mov|call|mov|call|mov|mov|mov|mov|push|mov|call|push|mov|push|call|pop|pop|mov|mov|pop|pop|pop|pop|add|cmp|call|mov|pop|ret|
+disassembled code :
+push|mov|push|push|mov|push|push|sub|push|push|push|push|mov|mov|rep|pop|mov|xor|push|mov|mov|mov|mov|or|mov|mov|call|cmp|jae|mov|mov|jmp|mov|xor|mov|div|mov|mov|shr|cmp|ja|jmp|mov|mov|shr|mov|call|sub|mov|cmp|ja|mov|mov|shr|mov|add|mov|jmp|mov|call|mov|mov|push|mov|call|mov|mov|add|push|mov|call|mov|mov|mov|jmp|mov|mov|mov|mov|push|mov|call|mov|mov|add|push|mov|call|mov|mov|mov|jmp|
+
+end ?_Copy@?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAEXII@Z
